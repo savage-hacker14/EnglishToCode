410f6cdf4597a94d062ab4d416fa0916cb172451
jacob <jacob1576@gmail.com>
2019-06-19 18:03:56 -0400

    Minor fix

--- src/Tester.java
+++ src/Tester.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Tester {
+	// Key variable
+	private static final boolean outputCode = false;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -25,20 +27,22 @@ public class Tester {
 		Interpretor.createLineOfCode(c);
 		System.out.println(c.toString());
 		
-//		// Testing Program
-//		Program p = new Program();
-//		p.addCommand(c);
-//		
-//		// Testing FileCreator
-//		FileCreator fc = new FileCreator("Test", lang, p);
-//		try {
-//			fc.createCodeFile();
-//			System.out.println("Code created!");
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			System.out.println("Error: Failed to generate code!");
-//		}
-//		
-//		reader.close();
+		if (outputCode) {
+			// Testing Program
+			Program p = new Program();
+			p.addCommand(c);
+			
+			// Testing FileCreator
+			FileCreator fc = new FileCreator("Test", lang, p);
+			try {
+				fc.createCodeFile();
+				System.out.println("Code created!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error: Failed to generate code!");
+			}
+		}
+		
+		reader.close();
 	}
 }
